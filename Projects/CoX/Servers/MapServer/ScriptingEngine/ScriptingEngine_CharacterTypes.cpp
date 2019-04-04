de60fcbeb22a36085f857135f8c38b1aa46aff07
Eric <erpmario@segs.io>
2019-04-03 19:20:48 -0500

    Add documentation to `ScriptingEngine_CharacterTypes.cpp`
    Add Doxygen to the `register_CharacterTypes` function
    
    Add comments and TODOs throughout the function
    
    Fix formatting throughout the file

--- Projects/CoX/Servers/MapServer/ScriptingEngine/ScriptingEngine_CharacterTypes.cpp
+++ Projects/CoX/Servers/MapServer/ScriptingEngine/ScriptingEngine_CharacterTypes.cpp
@@ -15,8 +15,13 @@
 #include "ScriptingEngine.h"
 #include "MapInstance.h"
 
+/*!
+ * @brief Initializes types related to characters and gameplay for the Lua scripting engine. Creates the Player table
+ *        and defines the functions that can be called on it.
+ */
 void ScriptingEngine::register_CharacterTypes()
 {
+    // Clue
     m_private->m_lua.new_usertype<Clue>("Clue",
         sol::constructors<Clue()>(),
         "name", sol::property(&Clue::getName, &Clue::setName),
@@ -25,7 +30,8 @@ void ScriptingEngine::register_CharacterTypes()
         "iconFile", sol::property(&Clue::getIconFile, &Clue::setIconFile)
     );
 
-    m_private->m_lua.new_usertype<Contact>( "Contact",
+    // Contact
+    m_private->m_lua.new_usertype<Contact>("Contact",
         // 3 constructors
         sol::constructors<Contact()>(),
         // gets or set the value using member variable syntax
@@ -47,6 +53,7 @@ void ScriptingEngine::register_CharacterTypes()
         "settingTitle", &Contact::m_setting_title
     );
 
+    // Destination
     m_private->m_lua.new_usertype<Destination>("Destination",
         sol::constructors<Destination()>(),
         "pointIdx", &Destination::point_idx,
@@ -55,25 +62,28 @@ void ScriptingEngine::register_CharacterTypes()
         "mapName", sol::property(&Destination::getLocationMapName, &Destination::setLocationMapName)
     );
 
-    m_private->m_lua.new_usertype<HideAndSeek>( "HideAndSeek",
+    // Hide and Seek
+    m_private->m_lua.new_usertype<HideAndSeek>("HideAndSeek",
         sol::constructors<HideAndSeek()>(),
         "foundCount", &HideAndSeek::m_found_count
     );
 
-    m_private->m_lua.new_usertype<Hunt>( "Hunt",
+    // Hunt
+    m_private->m_lua.new_usertype<Hunt>("Hunt",
         sol::constructors<Hunt()>(),
         "type", sol::property(&Hunt::getTypeString, &Hunt::setTypeString),
         "count", &Hunt::m_count
     );
 
-
-    m_private->m_lua.new_usertype<RelayRaceResult>( "RelayRaceResult",
+    // Relay Race Result
+    m_private->m_lua.new_usertype<RelayRaceResult>("RelayRaceResult",
         sol::constructors<RelayRaceResult()>(),
         "segment", &RelayRaceResult::m_segment,
         "lastTime", &RelayRaceResult::m_last_time,
         "bestTime", &RelayRaceResult::m_best_time
     );
 
+    // Souvenir
     m_private->m_lua.new_usertype<Souvenir>("Souvenir",
         sol::constructors<Souvenir()>(),
         "name", sol::property(&Souvenir::getName, &Souvenir::setName),
@@ -81,236 +91,290 @@ void ScriptingEngine::register_CharacterTypes()
         "icon", sol::property(&Souvenir::getIcon, &Souvenir::setIcon)
     );
 
+    // Task Entry
     m_private->m_lua.new_usertype<TaskEntry>("TaskEntry",
-          sol::constructors<TaskEntry()>(),
-          "dbId", &TaskEntry::m_db_id,
-          "taskList", &TaskEntry::m_task_list,
-          "resetSelectedTask", &TaskEntry::m_task_list);
+        sol::constructors<TaskEntry()>(),
+        "dbId", &TaskEntry::m_db_id,
+        "taskList", &TaskEntry::m_task_list,
+        "resetSelectedTask", &TaskEntry::m_task_list
+    );
 
-      m_private->m_lua.new_usertype<Task>("Task",
-          sol::constructors<Task()>(),
-          "dbId", &Task::m_db_id,
-          "description", sol::property(&Task::getDescription, &Task::setDescription),
-          "owner", sol::property(&Task::getOwner, &Task::setOwner),
-          "detail", sol::property(&Task::getDetail, &Task::setDetail),
-          "state", sol::property(&Task::getState, &Task::setState),
-          "isComplete", &Task::m_is_complete,
-          "inProgressMaybe", &Task::m_in_progress_maybe,
-          "hasLocation", &Task::m_has_location,
-          "isDetailInvalid", &Task::m_detail_invalid,
-          "location", &Task::m_location,
-          "finishTime", &Task::m_finish_time,
-          "taskIdx", &Task::m_task_idx,
-          "isAbandoned", &Task::m_is_abandoned,
-          "unknownInt1", &Task::m_unknown_1,
-          "unknownInt2", &Task::m_unknown_2,
-          "boardTrain", &Task::m_board_train
-      );
+    // Task
+    m_private->m_lua.new_usertype<Task>("Task",
+        sol::constructors<Task()>(),
+        "dbId", &Task::m_db_id,
+        "description", sol::property(&Task::getDescription, &Task::setDescription),
+        "owner", sol::property(&Task::getOwner, &Task::setOwner),
+        "detail", sol::property(&Task::getDetail, &Task::setDetail),
+        "state", sol::property(&Task::getState, &Task::setState),
+        "isComplete", &Task::m_is_complete,
+        "inProgressMaybe", &Task::m_in_progress_maybe,
+        "hasLocation", &Task::m_has_location,
+        "isDetailInvalid", &Task::m_detail_invalid,
+        "location", &Task::m_location,
+        "finishTime", &Task::m_finish_time,
+        "taskIdx", &Task::m_task_idx,
+        "isAbandoned", &Task::m_is_abandoned,
+        "unknownInt1", &Task::m_unknown_1,
+        "unknownInt2", &Task::m_unknown_2,
+        "boardTrain", &Task::m_board_train
+    );
 
-      // Player Object
-      m_private->m_lua["Player"] = m_private->m_lua.create_table(); // Empty Table aka Object
-      m_private->m_lua["Player"]["ClearTarget"] = [this]()
-      {
-          setTarget(*cl->m_ent, cl->m_ent->m_idx);
-      };
+    /* ************* *
+     * Player Object *
+     * ************* */
 
-      m_private->m_lua["Player"]["CloseContactDialog"] = [this]()
-      {
-          sendContactDialogClose(*cl);
-      };
+    // Create the Player Object itself.
+    m_private->m_lua["Player"] = m_private->m_lua.create_table(); // Empty Table aka Object
 
-      m_private->m_lua["Player"]["SetDebt"] = [this](const int debt)
-      {
-          setDebt(*cl->m_ent->m_char, debt);
-      };
+    // Clear the Player's current target so they are no longer targeting anything.
+    m_private->m_lua["Player"]["ClearTarget"] = [this]()
+    {
+        setTarget(*cl->m_ent, cl->m_ent->m_idx);
+    };
 
-      m_private->m_lua["Player"]["GiveDebt"] = [this](const int debt)
-      {
-          giveDebt(*cl, debt);
-      };
+    // Close the dialog window the Player currently has with a Contact.
+    m_private->m_lua["Player"]["CloseContactDialog"] = [this]()
+    {
+        sendContactDialogClose(*cl);
+    };
 
-      m_private->m_lua["Player"]["GiveEnhancement"] = [this](const char* name, int level)
-      {
-          QString e_name = QString::fromUtf8(name);
-          giveEnhancement(*cl, e_name, level);
-      };
+    // Set the Player's XP Debt to the passed value.
+    // TODO: Handle conversion from signed to unsigned int.
+    m_private->m_lua["Player"]["SetDebt"] = [this](const int debt)
+    {
+        setDebt(*cl->m_ent->m_char, debt);
+    };
 
-      m_private->m_lua["Player"]["GiveEnd"] = [this](const float end)
-      {
-          giveEnd(*cl, end);
-      };
+    // Give the Player an amount of XP Debt equal to the passed value.
+    m_private->m_lua["Player"]["GiveDebt"] = [this](const int debt)
+    {
+        giveDebt(*cl, debt);
+    };
 
-      m_private->m_lua["Player"]["SetEnd"] = [this](const float end)
-      {
-          setEnd(*cl->m_ent->m_char, end);
-      };
+    // Give the Player the specified Enchancement at the specified Level.
+    m_private->m_lua["Player"]["GiveEnhancement"] = [this](const char *name, int level)
+    {
+        QString e_name = QString::fromUtf8(name);
+        giveEnhancement(*cl, e_name, level);
+    };
 
-      m_private->m_lua["Player"]["SetHp"] = [this](const float hp)
-      {
-          setHP(*cl->m_ent->m_char, hp);
-      };
+    // Give the Player an amount of Endurance equal to the passed value.
+    m_private->m_lua["Player"]["GiveEnd"] = [this](const float end)
+    {
+        giveEnd(*cl, end);
+    };
 
-      m_private->m_lua["Player"]["GiveHp"] = [this](const float hp)
-      {
-          giveHp(*cl, hp);
-      };
+    // Set the Player's Endurance to the passed value.
+    m_private->m_lua["Player"]["SetEnd"] = [this](const float end)
+    {
+        setEnd(*cl->m_ent->m_char, end);
+    };
 
-      m_private->m_lua["Player"]["SetInf"] = [this](const int inf)
-      {
-          setInf(*cl->m_ent->m_char, inf);
-      };
+    // Set the Player's HP to the passed value.
+    m_private->m_lua["Player"]["SetHp"] = [this](const float hp)
+    {
+        setHP(*cl->m_ent->m_char, hp);
+    };
 
-      m_private->m_lua["Player"]["GiveInf"] = [this](const int inf)
-      {
-          modifyInf(*cl, inf);
-      };
+    // Give the Player an amount of HP equal to the passed value.
+    m_private->m_lua["Player"]["GiveHp"] = [this](const float hp)
+    {
+        giveHp(*cl, hp);
+    };
 
-      m_private->m_lua["Player"]["GiveInsp"] = [this](const char* name)
-      {
-          QString e_name = QString::fromUtf8(name);
-          giveInsp(*cl, e_name);
-      };
+    // Set the Player's Influence to the passed value.
+    // TODO: Handle conversion from signed to unsigned int.
+    m_private->m_lua["Player"]["SetInf"] = [this](const int inf)
+    {
+        setInf(*cl->m_ent->m_char, inf);
+    };
 
-      m_private->m_lua["Player"]["SetXp"] = [this](const int xp)
-      {
-          setXP(*cl->m_ent->m_char, xp);
-      };
+    // Give the Player an amount of Influence equal to the passed value.
+    m_private->m_lua["Player"]["GiveInf"] = [this](const int inf)
+    {
+        modifyInf(*cl, inf);
+    };
 
-      m_private->m_lua["Player"]["GiveXp"] = [this](const int xp)
-      {
-          giveXp(*cl, xp);
-      };
+    // Give the Player the specified Inspiration.
+    m_private->m_lua["Player"]["GiveInsp"] = [this](const char *name)
+    {
+        QString e_name = QString::fromUtf8(name);
+        giveInsp(*cl, e_name);
+    };
 
-      m_private->m_lua["Player"]["SendFloatingDamage"] = [this](const int tgt_idx, const int amount)
-      {
-          sendFloatingNumbers(*cl, tgt_idx, amount);
-      };
+    // Set the Player's XP to the passed value.
+    // TODO: Handle conversion from signed to unsigned int.
+    m_private->m_lua["Player"]["SetXp"] = [this](const int xp)
+    {
+        setXP(*cl->m_ent->m_char, xp);
+    };
 
-      m_private->m_lua["Player"]["FaceEntity"] = [this](const int tgt_idx)
-      {
-          sendFaceEntity(*cl, tgt_idx);
-      };
+    // Give the Player an amount of XP equal to the passed value.
+    // TODO: Handle conversion from signed to unsigned int.
+    m_private->m_lua["Player"]["GiveXp"] = [this](const int xp)
+    {
+        giveXp(*cl, xp);
+    };
 
-      m_private->m_lua["Player"]["FaceLocation"] = [this](glm::vec3 &loc)
-      {
-          sendFaceLocation(*cl, loc);
-      };
+    // Send the specified number of floating damage numbers to display over the specified target.
+    // TODO: Handle conversion from signed to unsigned int.
+    m_private->m_lua["Player"]["SendFloatingDamage"] = [this](const int tgt_idx, const int amount)
+    {
+        sendFloatingNumbers(*cl, tgt_idx, amount);
+    };
 
-      m_private->m_lua["Player"]["AddUpdateContact"] = [this](const Contact &contact)
-      {
-          updateContactStatusList(*cl, contact);
-      };
+    // Cause the Player to face the specified target.
+    // TODO: Handle conversion from signed to unsigned int.
+    m_private->m_lua["Player"]["FaceEntity"] = [this](const int tgt_idx)
+    {
+        sendFaceEntity(*cl, tgt_idx);
+    };
 
-      m_private->m_lua["Player"]["SetActiveDialogCallback"] = [this](std::function<void(int)> callback)
-      {
-          cl->m_ent->setActiveDialogCallback(callback);
-      };
+    // Cause the Player to face the specified location.
+    m_private->m_lua["Player"]["FaceLocation"] = [this](glm::vec3 &loc)
+    {
+        sendFaceLocation(*cl, loc);
+    };
 
-      m_private->m_lua["Player"]["RemoveContact"] = [this](const Contact &contact)
-      {
-          removeContact(*cl, contact);
-      };
+    // Add the specified Contact to the Player's Contact list.
+    m_private->m_lua["Player"]["AddUpdateContact"] = [this](const Contact &contact)
+    {
+        updateContactStatusList(*cl, contact);
+    };
 
-      m_private->m_lua["Player"]["UpdateTaskDetail"] = [this](const Task &task)
-      {
-          updateTaskDetail(*cl, task);
-      };
+    // Define the function that will be called with the dialog button is pressed.
+    m_private->m_lua["Player"]["SetActiveDialogCallback"] = [this](std::function<void(int)> callback)
+    {
+        cl->m_ent->setActiveDialogCallback(callback);
+    };
 
-      m_private->m_lua["Player"]["AddListOfTasks"] = [this](const sol::as_table_t<std::vector<Task>> task_list)
-      {
-          const auto& listMap = task_list.source;
-          vTaskList listToSend;
-          for (const auto& kvp : listMap)
-          {
-              listToSend.push_back(kvp);
-          }
-          addListOfTasks(cl, listToSend);
-      };
+    // Remove the specified Contact from the Player's Contact list.
+    m_private->m_lua["Player"]["RemoveContact"] = [this](const Contact &contact)
+    {
+        removeContact(*cl, contact);
+    };
 
-      m_private->m_lua["Player"]["AddUpdateTask"] = [this](const Task &task)
-      {
-          sendUpdateTaskStatusList(*cl, task);
-      };
+    // Send the specified task's details to the Player.
+    m_private->m_lua["Player"]["UpdateTaskDetail"] = [this](const Task &task)
+    {
+        updateTaskDetail(*cl, task);
+    };
 
-      m_private->m_lua["Player"]["RemoveTask"] = [this](const Task &task)
-      {
-          removeTask(*cl, task);
-      };
+    // Add the specified list of tasks to the Player's task list.
+    m_private->m_lua["Player"]["AddListOfTasks"] = [this](const sol::as_table_t<std::vector<Task>> task_list)
+    {
+        const auto& listMap = task_list.source;
+        vTaskList listToSend;
+        for (const auto& kvp : listMap)
+        {
+          listToSend.push_back(kvp);
+        }
+        addListOfTasks(cl, listToSend);
+    };
 
-      m_private->m_lua["Player"]["SelectTask"] = [this](const Task &task)
-      {
-          selectTask(*cl, task);
-      };
+    // Add the specified task to the Player's task list.
+    m_private->m_lua["Player"]["AddUpdateTask"] = [this](const Task &task)
+    {
+        sendUpdateTaskStatusList(*cl, task);
+    };
 
-      m_private->m_lua["Player"]["StartMissionTimer"] = [this](const char* message, float timer)
-      {
-          QString mess = QString::fromUtf8(message);
-          sendMissionObjectiveTimer(*cl, mess, timer);
-      };
+    // Remove the specified task from the Player's task list.
+    m_private->m_lua["Player"]["RemoveTask"] = [this](const Task &task)
+    {
+        removeTask(*cl, task);
+    };
 
-      m_private->m_lua["Player"]["SetWaypoint"] = [this](const int point_idx, glm::vec3 loc)
-      {
-          sendWaypoint(*cl, point_idx, loc);
-      };
+    // Make the specified task the Player's "selected task," thus making it appear red on the compass and map.
+    m_private->m_lua["Player"]["SelectTask"] = [this](const Task &task)
+    {
+        selectTask(*cl, task);
+    };
 
-      m_private->m_lua["Player"]["LevelUp"] = [this]()
-      {
-          playerTrain(*cl);
-      };
+    // Start a timer for the specified length with the specified message.
+    m_private->m_lua["Player"]["StartMissionTimer"] = [this](const char *message, float timer)
+    {
+        QString mess = QString::fromUtf8(message);
+        sendMissionObjectiveTimer(*cl, mess, timer);
+    };
 
-      m_private->m_lua["Player"]["OpenTitleMenu"] = [this]()
-      {
-          QString origin = getOriginTitle(*cl->m_ent->m_char);
-          setTitle(*cl, origin);
-      };
+    // Sets the Player's waypoint to the specifed location.
+    m_private->m_lua["Player"]["SetWaypoint"] = [this](const int point_idx, glm::vec3 loc)
+    {
+        sendWaypoint(*cl, point_idx, loc);
+    };
 
-      m_private->m_lua["Player"]["GiveTempPower"] = [this](const char* power)
-      {
-          giveTempPower(cl, power);
-      };
+    // Causes the Player to immediately level up.
+    m_private->m_lua["Player"]["LevelUp"] = [this]()
+    {
+        playerTrain(*cl);
+    };
 
-      m_private->m_lua["Player"]["AddClue"] = [this](const Clue clue)
-      {
-          addClue(*cl, clue);
-      };
+    // Open the Player's title selection window. Currently seems to only set the Player's title to their origin title.
+    m_private->m_lua["Player"]["OpenTitleMenu"] = [this]()
+    {
+        QString origin = getOriginTitle(*cl->m_ent->m_char);
+        setTitle(*cl, origin);
+    };
 
-      m_private->m_lua["Player"]["RemoveClue"] = [this](const Clue clue)
-      {
-          removeClue(*cl, clue);
-      };
+    // Give the Player the specified temporary power.
+    m_private->m_lua["Player"]["GiveTempPower"] = [this](const char *power)
+    {
+        giveTempPower(cl, power);
+    };
 
-      m_private->m_lua["Player"]["AddSouvenir"] = [this](const Souvenir souvenir)
-      {
-          addSouvenir(*cl, souvenir);
-      };
+    // Add the specified Clue to the Player's inventory.
+    m_private->m_lua["Player"]["AddClue"] = [this](const Clue clue)
+    {
+        addClue(*cl, clue);
+    };
 
-      m_private->m_lua["Player"]["RemoveSouvenir"] = [this](const Souvenir souvenir)
-      {
-          removeSouvenir(*cl, souvenir);
-      };
+    // Remove the specified Clue from the Player's inventory.
+    m_private->m_lua["Player"]["RemoveClue"] = [this](const Clue clue)
+    {
+        removeClue(*cl, clue);
+    };
 
-      m_private->m_lua["Player"]["Revive"] = [this](const int revive_lvl)
-      {
-          revive(cl, revive_lvl);
-      };
+    // Add the specified Souvenir from the Player's inventory.
+    m_private->m_lua["Player"]["AddSouvenir"] = [this](const Souvenir souvenir)
+    {
+        addSouvenir(*cl, souvenir);
+    };
 
-      m_private->m_lua["Player"]["Respawn"] = [this](const char* loc_name)
-      {
-          respawn(*cl, loc_name);
-      };
+    // Remove the specified Souvenir from the Player's inventory.
+    m_private->m_lua["Player"]["RemoveSouvenir"] = [this](const Souvenir souvenir)
+    {
+        removeSouvenir(*cl, souvenir);
+    };
 
-      m_private->m_lua["Player"]["AddHideAndSeekPoint"] = [this](int points)
-      {
-          addHideAndSeekResult(*cl, points);
-      };
+    // Immediately revive the Player on the spot without returning to a Hospital. Different values for revive_lvl
+    // result in different restoration values for HP and Endurance.
+    m_private->m_lua["Player"]["Revive"] = [this](const int revive_lvl)
+    {
+        revive(cl, revive_lvl);
+    };
 
-      m_private->m_lua["Player"]["AddRelayRaceResult"] = [this](RelayRaceResult *raceResult)
-      {
-          addRelayRaceResult(*cl, *raceResult);
-      };
+    // Immediately respawn the player at the specified location.
+    m_private->m_lua["Player"]["Respawn"] = [this](const char *loc_name)
+    {
+        respawn(*cl, loc_name);
+    };
 
-      m_private->m_lua["Player"]["GetRelayRaceResult"] = [this](int segment)
-      {
-          return getRelayRaceResult(*cl, segment);
-      };
+    // Add the specified number of points to the Player's Hide and Seek score.
+    m_private->m_lua["Player"]["AddHideAndSeekPoint"] = [this](int points)
+    {
+        addHideAndSeekResult(*cl, points);
+    };
+
+    // Add the specified Relay Race result to the Player.
+    m_private->m_lua["Player"]["AddRelayRaceResult"] = [this](RelayRaceResult *raceResult)
+    {
+        addRelayRaceResult(*cl, *raceResult);
+    };
+
+    // Get the specified Relay Race result from the Player.
+    m_private->m_lua["Player"]["GetRelayRaceResult"] = [this](int segment)
+    {
+        return getRelayRaceResult(*cl, segment);
+    };
 }
